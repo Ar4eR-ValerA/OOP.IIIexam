ecc3aa6cc25d5b05ad46c6617191ef5be065e631
Winter091 <winter091@yandex.ru>
2022-01-22 15:39:27 +0300

    Add RideEntity repository

---
+++ Taksi.Server/DAL/Repositories/Implementations/Ef/EfRideRepository.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
+using Taksi.DTO.Enums;
+using Taksi.Server.DAL.Entities;
+
+namespace Taksi.Server.DAL.Repositories.Implementations.Ef
+{
+    public class EfRideRepository : EfRepository<RideEntity>
+    {
+        public EfRideRepository(DbContext context, DbSet<RideEntity> container)
+            : base(context, container)
+        {
+        }
+
+        public new async Task<RideEntity> GetByIdAsync(Guid id)
+        {
+            return await GetDbSet().FirstOrDefaultAsync(entity => entity.Id.Equals(id));
+        }
+
+        public new async Task<IEnumerable<RideEntity>> GetAllAsync()
+        {
+            return await GetDbSet().ToListAsync();
+        }
+
+        public new async Task<IEnumerable<RideEntity>> GetWhereAsync(Func<RideEntity, bool> predicate)
+        {
+            return await GetDbSet().Where(predicate).AsQueryable().ToListAsync();
+        }
+
+        private IIncludableQueryable<RideEntity, RideStatus> GetDbSet()
+        {
+            return DbSetContainer.Include(entity => entity.Status);   
+        }
+    }
+}
