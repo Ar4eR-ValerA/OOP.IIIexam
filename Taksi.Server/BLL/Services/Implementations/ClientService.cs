fa6376e5be4ba3a2e27c7d806d867a22df58afc0
DaniilGumin <gumin.danya@gmail.com>
2022-01-23 17:59:23 +0300

    feat: add log for ClientService

--- Taksi.Server/BLL/Services/Implementations/ClientService.cs
+++ Taksi.Server/BLL/Services/Implementations/ClientService.cs
@@ -5,6 +5,7 @@ using Taksi.Server.BLL.Services.Interfaces;
 using Taksi.Server.DAL.Entities;
 using Taksi.Server.DAL.Exceptions;
 using Taksi.Server.DAL.Repositories.Interfaces;
+using Taksi.Server.Logger;
 
 namespace Taksi.Server.BLL.Services.Implementations
 {
@@ -12,36 +13,49 @@ namespace Taksi.Server.BLL.Services.Implementations
     {
         private readonly IRepository<ClientEntity> _clientRepository;
         private readonly IRepository<CreditCardEntity> _creditCardRepository;
+        private readonly ILogger _logger;
 
-        public ClientService(IRepository<ClientEntity> clientRepo, IRepository<CreditCardEntity> creditCardRepo)
+        public ClientService(IRepository<ClientEntity> clientRepo, IRepository<CreditCardEntity> creditCardRepo, ILogger logger)
         {
             _clientRepository = clientRepo ?? throw new ArgumentNullException(nameof(clientRepo));
             _creditCardRepository = creditCardRepo ?? throw new ArgumentNullException(nameof(creditCardRepo));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
         }
 
         public async Task RegisterClient(ClientEntity clientEntity)
         {
+            _logger.LogInfo($"Client {clientEntity.Id} registered.");
+            
             await _clientRepository.InsertAsync(clientEntity);
         }
 
         public async Task UnregisterClient(Guid clientId)
         {
+            _logger.LogInfo($"Client {clientId} unregistered.");
+            
             await _clientRepository.RemoveAsync(clientId);
         }
 
         public async Task RegisterCreditCard(CreditCardEntity creditCardEntity)
         {
+            _logger.LogInfo($"Registered credit card {creditCardEntity.Id} for client {creditCardEntity.ClientId}.");
+            
             await _creditCardRepository.InsertAsync(creditCardEntity);
         }
 
         public async Task UnregisterCreditCard(Guid creditCardId)
         {
+            _logger.LogInfo($"Credit card {creditCardId} unregistered.");
+            
             await _creditCardRepository.RemoveAsync(creditCardId);
         }
 
         public bool HasCreditCard(Guid clientId)
         {
             var cards = _creditCardRepository.GetWhereAsync(card => card.ClientId == clientId);
+            
+            _logger.LogInfo($"Check credit card existing for client {clientId}");
+            
             return cards.Any();
         }
 
@@ -50,6 +64,9 @@ namespace Taksi.Server.BLL.Services.Implementations
             if (!HasCreditCard(clientId))
                 throw new EntityDoesNotExistException("Current client doesn't have credit card");
             var cards = _creditCardRepository.GetWhereAsync(card => card.ClientId == clientId);
+            
+            _logger.LogInfo($"Get balance for client {clientId}");
+            
             return cards.Last().CardBalance;
         }
 
@@ -59,6 +76,9 @@ namespace Taksi.Server.BLL.Services.Implementations
                 throw new EntityDoesNotExistException("Current client doesn't have credit card");
             var cards = _creditCardRepository.GetWhereAsync(card => card.ClientId == clientId);
             cards.Last().CardBalance = newBalance;
+            
+            _logger.LogInfo($"Set balance for client {clientId}");
+
             _creditCardRepository.UpdateAsync(cards.Last());
         }
     }
