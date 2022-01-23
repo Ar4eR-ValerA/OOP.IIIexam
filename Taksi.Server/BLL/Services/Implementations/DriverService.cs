32a55f41b148358a61d3ee4d353402b0fc9539a1
DaniilGumin <gumin.danya@gmail.com>
2022-01-23 17:49:44 +0300

    feat: add log for DriverService

--- Taksi.Server/BLL/Services/Implementations/DriverService.cs
+++ Taksi.Server/BLL/Services/Implementations/DriverService.cs
@@ -6,25 +6,33 @@ using Taksi.Server.BLL.Services.Interfaces;
 using Taksi.Server.DAL.Entities;
 using Taksi.Server.DAL.Exceptions;
 using Taksi.Server.DAL.Repositories.Interfaces;
+using Taksi.Server.Logger;
 
 namespace Taksi.Server.BLL.Services.Implementations
 {
     public class DriverService : IDriverService
     {
         private readonly IRepository<DriverEntity> _driverRepository;
+        private readonly ILogger _logger;
 
-        public DriverService(IRepository<DriverEntity> driverRepo)
+        public DriverService(IRepository<DriverEntity> driverRepo, ILogger logger)
         {
             _driverRepository = driverRepo ?? throw new ArgumentNullException(nameof(driverRepo));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            
         }
 
         public async Task RegisterDriver(DriverEntity driverEntity)
         {
+            _logger.LogInfo($"Driver {driverEntity.Id} registered.");
+            
             await _driverRepository.InsertAsync(driverEntity);
         }
 
         public async Task UnregisterDriver(Guid driverId)
         {
+            _logger.LogInfo($"Driver {driverId} unregistered.");
+            
             await _driverRepository.RemoveAsync(driverId);
         }
 
@@ -35,6 +43,8 @@ namespace Taksi.Server.BLL.Services.Implementations
             if (driver.CountOfRatings == 0)
                 return 0;
 
+            _logger.LogInfo($"Get driver {driverId} rating.");
+            
             return driver.RatingSum / driver.CountOfRatings;
         }
 
@@ -43,6 +53,9 @@ namespace Taksi.Server.BLL.Services.Implementations
             var driver = await _driverRepository.GetByIdAsync(driverId);
             driver.RatingSum += score;
             driver.CountOfRatings++;
+            
+            _logger.LogInfo($"Client rate driver {driverId}.");
+            
             await _driverRepository.UpdateAsync(driver);
         }
 
@@ -52,12 +65,18 @@ namespace Taksi.Server.BLL.Services.Implementations
             driver.Location ??= newLocation;
             driver.Location.X = newLocation.X;
             driver.Location.Y = newLocation.Y;
+            
+            _logger.LogInfo($"Driver {driverId} update location.");
+            
             await _driverRepository.UpdateAsync(driver);
         }
 
         public async Task<Point2dEntity> GetLocation(Guid driverId)
         {
             var driver = await _driverRepository.GetByIdAsync(driverId);
+            
+            _logger.LogInfo($"Get driver {driverId} location.");
+            
             return driver.Location;
         }
 
@@ -65,18 +84,27 @@ namespace Taksi.Server.BLL.Services.Implementations
         {
             var driver = await _driverRepository.GetByIdAsync(driverId);
             driver.Status = newStatus;
+            
+            _logger.LogInfo($"Update driver {driverId} status.");
+            
             await _driverRepository.UpdateAsync(driver);
         }
 
         public async Task<DriverStatus> GetStatus(Guid driverId)
         {
             var driver = await _driverRepository.GetByIdAsync(driverId);
+            
+            _logger.LogInfo($"Get driver {driverId} status.");
+            
             return driver.Status;
         }
 
         public async Task<TaxiType> GetTaxiType(Guid driverId)
         {
             var driver = await _driverRepository.GetByIdAsync(driverId);
+            
+            _logger.LogInfo($"Get driver {driverId} taxi type.");
+            
             return driver.TaxiType;
         }
 
@@ -100,6 +128,8 @@ namespace Taksi.Server.BLL.Services.Implementations
                     nearestDriver = driver;
                 }
             }
+            
+            _logger.LogInfo($"Get nearest driver for location ({location.X};{location.Y}).");
 
             return nearestDriver.Id;
         }
